0bb8a8cc171aa864b652bdbd4d79ae3b8d329caa
Allart Kooiman <allartk@gmail.com>
2014-07-28 10:15:27 +0200

    set url as layer prop for online fallback

--- src/TileLayer.Offline.js
+++ src/TileLayer.Offline.js
@@ -4,18 +4,47 @@
  * and open the template in the editor.
  */
 L.TileLayer.Offline = L.TileLayer.Functional.extend({
-    initialize: function(options) {
-        var tileFunction = function(view) {            
-            var url = 'http://otile1.mqcdn.com/tiles/1.0.0/map/{0}/{1}/{2}.jpg'
-                    .replace('{0}', view.zoom)
-                    .replace('{1}', view.tile.row)
-                    .replace('{2}', view.tile.column)
-                    .replace('{3}', view.subdomain);
-            return url;
+    
+    initialize: function(url,options) {        
+        
+        var tileFunction = function(url,view) {            
+            var ObjectUrl = url
+                    .replace('{z}', view.zoom)
+                    .replace('{y}', view.tile.row)
+                    .replace('{x}', view.tile.column)
+                    .replace('{s}', view.subdomain);     
+            console.log(ObjectUrl);            
+            return ObjectUrl;
         }
         this._tileFunction = tileFunction;
-        L.TileLayer.prototype.initialize.call(this, null, options);
-    }
+        L.TileLayer.prototype.initialize.call(this, url, options);
+    },
+    getTileUrl: function (tilePoint) {
+      var map = this._map,
+        crs = map.options.crs,
+        tileSize = this.options.tileSize,
+        zoom = tilePoint.z,
+        nwPoint = tilePoint.multiplyBy(tileSize),
+        sePoint = nwPoint.add(new L.Point(tileSize, tileSize)),
+        nw = crs.project(map.unproject(nwPoint, zoom)),
+        se = crs.project(map.unproject(sePoint, zoom)),
+        bbox = [nw.x, se.y, se.x, nw.y].join(',');
+
+      // Setup object to send to tile function.
+      var view = {
+        bbox: bbox,
+        width: tileSize,
+        height: tileSize,
+        zoom: zoom,
+        tile: {
+          row: this.options.tms ? this._tileNumBounds.max.y - tilePoint.y : tilePoint.y,
+          column: tilePoint.x
+        },
+        subdomain: this._getSubdomain(tilePoint)
+      };
+      
+      return this._tileFunction(this._url,view);
+    }    
 
 });
 
