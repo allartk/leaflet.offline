90b825fba6039ffa3a907124b9c2c4f284e29dc5
allart <allartk@gmail.com>
2016-11-26 09:26:44 +0100

    create gulp task for dist

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('dist',['js','css','img']);
+
+
+gulp.task('js',function() {
+    gulp.src('src/**/*.js')
+    .pipe(concat('leaflet.offline.js'))
+    .pipe(gulp.dest('./dist/'));
+})
+
+gulp.task('css',function() {
+    gulp.src('src/**/*.css')
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('img',function() {
+    gulp.src('src/images/*')
+    .pipe(gulp.dest('./dist/images/'))
+})
