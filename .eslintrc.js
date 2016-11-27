4a02e430faf240e988b9de06721022f59a2abf59
allart <allartk@gmail.com>
2016-11-27 12:24:09 +0100

    Take eslint config of leaflet

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,22 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise"
-    ]
+    "extends": "mourner",
+    "rules": {
+        "no-mixed-spaces-and-tabs": [
+          2,
+          "smart-tabs"
+        ],
+        "indent": [
+          2,
+          "tab",
+          {
+            "VariableDeclarator": 0
+          }
+        ],
+        "curly": 2,
+        "spaced-comment": 2,
+        "strict": 0,
+        "wrap-iife": 0,
+        "key-spacing": 0,
+        "consistent-return": 0
+    }
 };
