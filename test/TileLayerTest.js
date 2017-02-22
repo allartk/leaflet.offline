34e6e313f808409f3fc868e501516de3c9880941
allart <allartk@gmail.com>
2017-02-22 09:10:44 +0100

    use subdomains in url

--- test/TileLayerTest.js
+++ test/TileLayerTest.js
@@ -23,6 +23,39 @@ describe('tilelayer', function () {
 		assert.lengthOf(tiles, 16);
 		var urls = tiles.map(function (t) { return t.url; });
 		assert.include(urls, 'http://a.tile.openstreetmap.org/16/33677/21651.png');
+		var keys = tiles.map(function (t) { return t.key; });
+		assert.include(keys, 'http://a.tile.openstreetmap.org/16/33677/21651.png');
 
+	});
+
+	it('calculates tile urls,keys at level 16 with subdomains', function () {
+		var layer = L.tileLayer.offline('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+		var bounds = new L.Bounds(
+			new L.Point(8621975, 5543267.999999999),
+			new L.Point(8621275, 5542538)
+		);
+		var tiles = layer.getTileUrls(bounds, 16);
+		assert.lengthOf(tiles, 16);
+		var urls = tiles.map(function (t) { return t.url.replace(/[abc]\./, ''); });
+		assert.include(urls, 'http://tile.openstreetmap.org/16/33677/21651.png');
+		var keys = tiles.map(function (t) { return t.key; });
+		assert.include(keys, 'http://a.tile.openstreetmap.org/16/33677/21651.png');
+	});
+
+	it('uses subdomains for url and not for key', function () {
+		var layer = L.tileLayer.offline('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+		var bounds = new L.Bounds(
+			new L.Point(8621975, 5543267.999999999),
+			new L.Point(8621275, 5542538)
+		);
+		var tiles = layer.getTileUrls(bounds, 16);
+		var subs = tiles.map(function (t) { return t.url.match(/([abc])\./)['1']; });
+		assert.include(subs, 'a');
+		assert.include(subs, 'b');
+		assert.include(subs, 'c');
+		var subskeys = tiles.map(function (t) { return t.key.match(/([abc])\./)['1']; });
+		assert.include(subskeys, 'a');
+		assert.notInclude(subskeys, 'b');
+		assert.notInclude(subskeys, 'c');
 	});
 });
