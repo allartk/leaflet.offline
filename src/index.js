fcce6147aac0aeb1b777bc1ee0d5eb8632c75b1e
allart <allartk@gmail.com>
2017-11-15 17:15:51 +0100

    fix eslint rules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-// import control from './ControlSaveTiles';
 import layer from './TileLayerOffline';
+// import control from './ControlSaveTiles';
+
 // require('./VectorGrid.Protobuf.Offline');
 
 // TODO export too!
