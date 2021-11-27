4c9a814500e8c671604275355650a1ec19764b2a
allartk <allartk@gmail.com>
2021-11-27 13:11:31 +0100

    Improve_examples (#106)
    * fix mobile menu
    
    * improve examples

--- docs/src/apilayer.js
+++ docs/src/apilayer.js
@@ -1,16 +1,14 @@
 import { getTile, downloadTile, saveTile } from 'leaflet.offline';
+import { wmtsUrlTemplate } from './const';
 
 /* global L */
 const map = L.map('map');
-
-const wmtsUrlTemplate = 'https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=EPSG:3857&layer={layer}&tilematrix={z}&tilerow={y}&tilecol={x}&format=image%2Fpng';
 
 const brtLayer = L.tileLayer(wmtsUrlTemplate, {
   layer: 'standaard',
   format: 'image/png',
   transparent: true,
 });
-
 
 brtLayer.addTo(map);
 
@@ -19,37 +17,35 @@ brtLayer.on('tileloadstart', (event) => {
   const url = tile.src;
   // reset tile.src, to not start download yet
   tile.src = '';
-  getTile(url)
-    .then((blob) => {
-      if (blob) {
-        tile.src = URL.createObjectURL(blob);
-        console.debug(`Loaded ${url} from idb`);
-        return;
-      }
-      tile.src = url;
-      // create helper function for it?
-      const { x, y, z } = event.coords;
-      const { _url: urlTemplate } = event.target;
-      const tileInfo = {
-        key: url,
-        url,
-        x,
-        y,
-        z,
-        urlTemplate,
-        createdAt: Date.now(),
-      };
-      downloadTile(url)
-        .then((dl) => saveTile(tileInfo, dl))
-        .then(() => console.debug(`Saved ${url} in idb`));
-    });
+  getTile(url).then((blob) => {
+    if (blob) {
+      tile.src = URL.createObjectURL(blob);
+      console.debug(`Loaded ${url} from idb`);
+      return;
+    }
+    tile.src = url;
+    // create helper function for it?
+    const { x, y, z } = event.coords;
+    const { _url: urlTemplate } = event.target;
+    const tileInfo = {
+      key: url,
+      url,
+      x,
+      y,
+      z,
+      urlTemplate,
+      createdAt: Date.now(),
+    };
+    downloadTile(url)
+      .then((dl) => saveTile(tileInfo, dl))
+      .then(() => console.debug(`Saved ${url} in idb`));
+  });
 });
-
 
 map.setView(
   {
-    lat: 52.090,
+    lat: 52.09,
     lng: 5.118,
   },
-  16,
+  16
 );
