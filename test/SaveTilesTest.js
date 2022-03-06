29e6f6ffb059fe49cf2658eb619d4f502907aece
allartk <allartk@gmail.com>
2022-03-06 12:21:19 +0100

    fix test imports

--- test/SaveTilesTest.js
+++ test/SaveTilesTest.js
@@ -1,34 +1,41 @@
 /* global describe, it, assert, beforeEach L sinon */
-import '../src/ControlSaveTiles';
-import {TileLayerOffline} from '../src/TileLayerOffline';
+import { savetiles } from '../src/ControlSaveTiles';
+import { TileLayerOffline } from '../src/TileLayerOffline';
 
 describe('control with defaults', () => {
   let c;
   let baseLayer;
   beforeEach(() => {
     const map = L.map(document.createElement('div'));
-    map.setView({
-      lat: 51.985,
-      lng: 5,
-    }, 16);
-    baseLayer = new TileLayerOffline('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      subdomains: 'abc',
-    }).addTo(map);
-    c = L.control.savetiles(baseLayer);
+    map.setView(
+      {
+        lat: 51.985,
+        lng: 5,
+      },
+      16
+    );
+    baseLayer = new TileLayerOffline(
+      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        subdomains: 'abc',
+      }
+    ).addTo(map);
+    c = savetiles(baseLayer);
     c.addTo(map);
     c._rmTiles();
   });
   it('exists', () => {
-    assert.ok(L.control.savetiles);
+    assert.ok(savetiles);
   });
   it('adds button', () => {
     const div = c.onAdd();
     assert.ok(div);
     assert.lengthOf(div.querySelectorAll('a'), 2);
   });
-  it('calculates storagesize', () => c.setStorageSize().then((n) => {
-    assert.equal(n, 0);
-  }));
+  it('calculates storagesize', () =>
+    c.setStorageSize().then((n) => {
+      assert.equal(n, 0);
+    }));
   it('_saveTiles sets status', () => {
     const stub = sinon.stub(c, '_loadTile').returns(Promise.resolve());
     const resetstub = sinon.stub(c, '_resetStatus');
@@ -51,27 +58,36 @@ describe('control with defaults', () => {
   it('_saveTiles calls loadTile for each tile', () => {
     const stub = sinon.stub(c, '_loadTile').returns(Promise.resolve());
     c._saveTiles();
-    assert.equal(stub.callCount, 1, `_loadTile has been called ${stub.callCount} times`);
+    assert.equal(
+      stub.callCount,
+      1,
+      `_loadTile has been called ${stub.callCount} times`
+    );
     stub.resetBehavior();
   });
 });
-
 
 describe('control with different options', () => {
   let map;
   let baseLayer;
   beforeEach(() => {
     map = L.map(document.createElement('div'));
-    map.setView({
-      lat: 51.985,
-      lng: 5,
-    }, 16);
-    baseLayer = new TileLayerOffline('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      subdomains: 'abc',
-    }).addTo(map);
+    map.setView(
+      {
+        lat: 51.985,
+        lng: 5,
+      },
+      16
+    );
+    baseLayer = new TileLayerOffline(
+      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        subdomains: 'abc',
+      }
+    ).addTo(map);
   });
   it('_saveTiles calculates tiles for 2 zoomlevels', () => {
-    const c = L.control.savetiles(baseLayer, {
+    const c = savetiles(baseLayer, {
       zoomlevels: [16, 17],
     });
     c.addTo(map);
@@ -84,7 +100,7 @@ describe('control with different options', () => {
     stub.resetBehavior();
   });
   it('_saveTiles calcs tiles for saveWhatYouSee', () => {
-    const c = L.control.savetiles(baseLayer, {
+    const c = savetiles(baseLayer, {
       saveWhatYouSee: true,
     });
     c.addTo(map);
@@ -93,25 +109,21 @@ describe('control with different options', () => {
     c._saveTiles();
     assert.isObject(c.status);
     assert.isArray(c.status._tilesforSave);
-    assert.equal(stub.callCount, 4, `_loadTile has been called ${stub.callCount} times`);
+    assert.equal(
+      stub.callCount,
+      4,
+      `_loadTile has been called ${stub.callCount} times`
+    );
     stub.resetBehavior();
   });
   it('calls confirm', () => {
     const callback = sinon.spy();
-    const c = L.control.savetiles(baseLayer, {
+    const c = savetiles(baseLayer, {
       confirm: callback,
     });
     c.addTo(map);
     c._rmTiles();
     c._saveTiles();
     assert(callback.calledOnce);
-  });
-  it('does not set unkown option', () => {
-    const c = L.control.savetiles(baseLayer);
-    assert.throws(() => c.setOption('aslkdjnvssdfkjn', 'lkjnsdfkvljsnsdfv'));
-  });
-  it('does set  option', () => {
-    const c = L.control.savetiles(baseLayer);
-    assert.doesNotThrow(() => c.setOption('saveWhatYouSee', true));
   });
 });
