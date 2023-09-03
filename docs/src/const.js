6bb228312eb01d680ba6340a5c33550e3215b6ca
Grant <github@firefishy.com>
2023-09-03 10:36:28 +0100

    Update const.js to use correct tile.osm.org URL (#313)
    See: https://github.com/openstreetmap/operations/issues/737

--- docs/src/const.js
+++ docs/src/const.js
@@ -1,3 +1,3 @@
-export const urlTemplate = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+export const urlTemplate = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 export const wmtsUrlTemplate =
   'https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=EPSG:3857&layer={layer}&tilematrix={z}&tilerow={y}&tilecol={x}&format=image%2Fpng';
